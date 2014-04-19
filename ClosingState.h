e10c90a294572e38d49e1453386165ef8e4fe7e8
joevasak <jvasak@bell.ent>
2014-04-19 14:05:37 -0400

    Update ClosingState.h

--- ClosingState.h
+++ ClosingState.h
@@ -2,14 +2,16 @@ class ClosingState : public GarageOpenerState
 {
 	public:
 		ClosingState(char value[]) : GarageOpenerState(value) {}
-	void ReadData(GarageOpener state) {state.setState(ClosingState("ClosingState"));}
-	void ResetMotor(GarageOpener state) {state.setState(ClosingState("ClosingState"));}
-	void MoveMoter(GarageOpener state) 
+	void ReadData(GarageOpener* state) {state->setState(ClosingState("ClosingState"));}
+	void ResetMotor(GarageOpener* state) {state->setState(ClosingState("ClosingState"));}
+	void MoveMoter(GarageOpener* state) 
 	{
-		state.setState(CloseState("CloseState"));
-		state.getCMotor().MoveMotor(-(int)state.getCSensor().getDistance());
-		state.getCDoorLatch().setLatchState(true);
+		state->setState(CloseState("CloseState"));
+		state->getCMotor().MoveMotor(-(int)state->getCSensor().getDistance());
+		CDoorLatch latch = state->getCDoorLatch();
+		latch.setLatchState(true);
+		state->setCDoorLatch(latch);
 	}
-	void DoorLatch(GarageOpener state) {state.setState(OpeningState("ClosingState"));}
-	bool Calibrate(GarageOpener state) {state.setState(ClosingState("ClosingState"));}
+	void DoorLatch(GarageOpener* state) {state->setState(OpeningState("ClosingState"));}
+	bool Calibrate(GarageOpener* state) {state->setState(ClosingState("ClosingState"));}
 };
